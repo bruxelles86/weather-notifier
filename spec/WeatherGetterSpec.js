55d144528f38f8aa90444bcc02ad029410760253
Will Gant <w_gant@yahoo.co.uk>
2017-07-31 13:35:59 +0000

    Completes failing test for weathergetter api thing

--- spec/WeatherGetterSpec.js
+++ spec/WeatherGetterSpec.js
@@ -3,10 +3,12 @@ const weatherGetter = require('../src/WeatherGetter.js')
 // request at a specific location- test for response containing location
 
 describe('weatherGetter', function() {
-  it('brings back weather data from api', function() {
+  it('brings back weather data from api', function(done) {
     var weatherGetter = new WeatherGetter();
     var location = 'Bruges';
-    weatherGetter.getApiData(location);
-    
-  });
+    weatherGetter.getApiData(location).then(function(apiResponse) {
+      expect(apiResponse).toBeTruthy();
+      done();
+    });
+  }, 5000);
 })

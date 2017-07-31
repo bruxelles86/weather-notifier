e5727723933385d8d2677da23fc2b47bb20a57ee
Robert Jones <robert.g.jones@gmail.com>
2017-07-31 15:18:21 +0100

    Return London weather data from API
    Data is requested from the world-weather online API, handled with a
    Promise. The Promise ensures that other, future functions wait for the
    api response to complete. This has been chosen over a callback because
    we intend to chain asynchronous events together.

--- spec/WeatherGetterSpec.js
+++ spec/WeatherGetterSpec.js
@@ -12,4 +12,3 @@ describe('weatherGetter', function() {
     });
   }, 5000);
 })
-
--- src/WeatherGetter.js
+++ src/WeatherGetter.js
@@ -1,17 +1,27 @@
+const http = require('http');
+
 function WeatherGetter() {
-    
+
 };
 
 WeatherGetter.prototype.getApiData = function() {
     var promise = new Promise(function(resolve, reject) {
-       
-       https://api.worldweatheronline.com/premium/v1/weather.ashx?key=d1f3365447a2494cb8b115449170706&q=London&format=json&num_of_days=20 
-        
-        
-        
-        
-    })
-    return promise
-}
+      var url = 'http://api.worldweatheronline.com/premium/v1/weather.ashx?key=d1f3365447a2494cb8b115449170706&q=London&format=json&num_of_days=20'
+
+
+      http.get(url, (res) => {
+        let rawData = '';
+        res.on('data', function(chunk) {
+          rawData += chunk;
+        });
+        res.on('end', function() {
+          resolve(rawData);
+        });
+      });
+
+
+    });
+    return promise;
+};
 
 module.exports = WeatherGetter;

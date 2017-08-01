0990a681cd86a3f7c8402f069f5f28ec8f72b484
Robert Jones <robert.g.jones@gmail.com>
2017-08-01 18:45:30 +0100

    Failing test: AlertMatcher can match alerts to api data

---
+++ spec/AlertMatcherSpec.js
@@ -0,0 +1,53 @@
+const AlertMatcher = require('../src/AlertMatcher.js');
+
+var apiStubLondon, apiStubParis, apiResponses, alert1, alert2
+
+apiStubLondon =
+{
+  data:
+  {
+    weather: [
+      {
+        maxtempC: 25,
+        date: "2017-08-20"
+      }
+    ],
+    request: [
+      {
+        query: "London, United Kingdom"
+      }
+    ]
+  }
+}
+
+apiStubParis =
+{
+  data:
+  {
+    weather: [
+      {
+        maxtempC: 19,
+        date: "2017-08-20"
+      }
+    ],
+    request: [
+      {
+        query: "Paris, France"
+      }
+    ]
+  }
+}
+apiResponses = [apiStubParis, apiStubLondon]
+
+alert1 = {locations: ['London'], dates: ["2017-08-20"], conditions: { temperature: 25 } }
+
+alert2 = {locations: ['Paris'], dates: ["2017-08-20"], conditions: { temperature: 25 } }
+
+alertsStub = [alert1, alert2]
+
+describe("AlertMatcher", function() {
+  it('match an alert with an api stub', function() {
+    alertMatcher = new AlertMatcher();
+    expect(alertMatcher.check(apiResponses, alertsStub)).toEqual([alert1]);
+  });
+});

8d56a0995ce8cbf14654f2087fa709756dc749f2
Will Gant <w_gant@yahoo.co.uk>
2017-08-02 11:41:09 +0000

    adds locationstoCheck function to interface

--- spec/InterfaceSpec.js
+++ spec/InterfaceSpec.js
@@ -4,6 +4,7 @@ describe('Interface', function() {
    it('ends up calling the notifier', function(){
       var interface = new Interface()
       interface.notifier = jasmine.createSpy("fakeNotifier")
+      interface.add('London', 15, '2017-08-20')
       interface.checkAlerts();
       expect(interface.notifier).toHaveBeenCalled();
    });
--- src/Interface.js
+++ src/Interface.js
@@ -1,4 +1,31 @@
- function Interface() {
+const Alert = require('./Alert.js')
+const AlertContainer = require('./AlertContainer.js')
+const AlertMatcher = require('./AlertMatcher.js')
+const Notifier = require('./Notifier.js')
+const WeatherGetter = require('./WeatherGetter.js')
+
+function Interface() {
+    this.notifier = new Notifier();
+    this.alertContainer = new AlertContainer();
+    this.alertMatcher = new AlertMatcher();
+    this.weatherGetter = new WeatherGetter();
+}
+
+Interface.prototype.add = function(location, temperature, date) {
+    this.alertContainer.create(new Alert(location, temperature, date));
+};
+
+Interface.prototype.locationsToCheck = function() {
+    var locations = []
+    this.alertContainer.forEach(function(alert) {
+        if(locations.includes(alert.location[0]) === false) {
+            locations.push(alert.location[0])
+        } 
+    });
+    return locations
+}
+
+Interface.prototype.checkAlerts = function() {
     
 }
 
